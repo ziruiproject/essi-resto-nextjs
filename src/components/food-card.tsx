e2293e1157fea2e88d82bea46fd9a141767bf154
Yudha Prawira <iniakureal@gmail.com>
2024-05-17 00:11:14 +0700

    add: food card component

--- src/components/food-card.tsx
+++ src/components/food-card.tsx
@@ -1,7 +1,20 @@
 import { Food } from "@/models/food";
+import { FoodImage } from "@/models/food-images";
+import Image from "next/image";
 
-export default function FoodCard(props: { food: Food }) {
-  return <div>
-
-  </div>;
+export default function FoodCard(props: { food: FoodImage }) {
+  const { food } = props;
+  return (
+    <a href={"/foods/" + food.id} key={food.id} className="flex flex-col">
+      <Image
+        src={"http://localhost:8000/storage/" + food.images[0].path}
+        alt={food.name}
+        width={512}
+        height={512}
+        className="rounded-3xl"
+      ></Image>
+      <h3 className="pt-3">{food.name}</h3>
+      <span className="font-semibold">{"Rp" + food.price}</span>
+    </a>
+  );
 }
